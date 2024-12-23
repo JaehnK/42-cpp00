39564c9d25d569fba74813a07fafa3eb46fb1991
Jaehun Kim <jaehukim@c2r17s2.42gyeongsan.kr>
2024-12-23 12:01:02 +0900

    [Feat] ex01

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,4 +1,20 @@
+#include "ex01.h"
+
 class Contact
 {
+	private:
+		std::string	FirstName;
+		std::string	LastName;
+		std::string NickName;
+		std::string	PhoneNumber;
+		std::string DarkestSecret;
 	
-}
+	public:
+		std::string	GetFirstName();
+		std::string	GetLastName();
+
+
+
+};
+
+Contact:: GetFirstName()
---
+++ ex01/ex01.h
@@ -0,0 +1,7 @@
+#ifndef __EX01__
+#define __EX01__
+
+#include <string>
+#include <iostream>
+
+#endif 
